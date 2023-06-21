48aecaabfedfffa93d4e957e889f555484522dd1
Nick Topacio <ntopacio25@gmail.com>
2023-06-21 17:09:50 -0400

    Add base application

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # bg-generator
+
+## About The Project
+
 A website that generates a background based on a palette chosen by the user.
+
+## Built With
+
+(What languages the project used)
+
+## Scope
+
+(What programming concepts or algorithms were used in this project)
+
+## Description
+
+(Any additional details about the project)
+
+## Contact
+
+Nick Topacio - ntopacio25@gmail.com
+
+## Acknowledgments
+
+(Any add-ons/plugins that were used, any sources that were used)
--- src/main.js
+++ src/main.js
@@ -1,3 +1,39 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
+import "../styles/components/bg.css";
 import "../styles/utils.css";
+
+function listenForButtonOne() {
+  let colorPalette = document.getElementById("bg__btn1");
+  colorPalette.addEventListener("click", () => {
+    console.log("clicked");
+    let colorPaletteOne = document.getElementById("bg__input1");
+    if (
+      colorPaletteOne.style.display === "none" ||
+      colorPaletteOne.style.display === ""
+    ) {
+      colorPaletteOne.style.setProperty("display", "block");
+    } else {
+      colorPaletteOne.style.setProperty("display", "none");
+    }
+  });
+}
+
+function listenForButtonTwo() {
+  let colorPalette = document.getElementById("bg__btn2");
+  colorPalette.addEventListener("click", () => {
+    console.log("clicked");
+    let colorPaletteTwo = document.getElementById("bg__input2");
+    if (
+      colorPaletteTwo.style.display === "none" ||
+      colorPaletteTwo.style.display === ""
+    ) {
+      colorPaletteTwo.style.setProperty("display", "block");
+    } else {
+      colorPaletteTwo.style.setProperty("display", "none");
+    }
+  });
+}
+
+listenForButtonOne();
+listenForButtonTwo();
