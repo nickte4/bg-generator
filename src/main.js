af184c15df92f66c7e628e478d1bede3d73acbd8
Nick Topacio <ntopacio25@gmail.com>
2023-06-21 19:30:27 -0400

    Added full functionality to site

--- src/main.js
+++ src/main.js
@@ -3,6 +3,32 @@ import "../styles/style.css";
 import "../styles/components/bg.css";
 import "../styles/utils.css";
 
+function setInitialColors() {
+  let colors = fetchColors();
+  document.body.style.background =
+    "radial-gradient(" + colors[0] + ", " + colors[1] + ")";
+}
+
+function fetchColors() {
+  if (!window.localStorage.getItem("color1")) {
+    window.localStorage.setItem("color1", "#ffffff");
+  }
+  if (!window.localStorage.getItem("color2")) {
+    window.localStorage.setItem("color2", "#000000");
+  }
+  let color1 = window.localStorage.getItem("color1");
+  let color2 = window.localStorage.getItem("color2");
+  return [color1, color2];
+}
+
+function setColorOne(color) {
+  window.localStorage.setItem("color1", color);
+}
+
+function setColorTwo(color) {
+  window.localStorage.setItem("color2", color);
+}
+
 function listenForButtonOne() {
   let colorPalette = document.getElementById("bg__btn1");
   colorPalette.addEventListener("click", () => {
@@ -35,5 +61,63 @@ function listenForButtonTwo() {
   });
 }
 
+function listenForColorPickerOne() {
+  let colorPicker = document.getElementById("bg__input1");
+  colorPicker.addEventListener("input", () => {
+    let chosenColor = colorPicker.value;
+    setColorOne(chosenColor);
+    let colors = fetchColors();
+    document.body.style.background =
+      "radial-gradient(" + chosenColor + ", " + colors[1] + ")";
+    console.log(chosenColor);
+  });
+}
+
+function listenForColorPickerTwo() {
+  let colorPicker = document.getElementById("bg__input2");
+  colorPicker.addEventListener("input", () => {
+    let chosenColor = colorPicker.value;
+    setColorTwo(chosenColor);
+    let colors = fetchColors();
+    document.body.style.background =
+      "radial-gradient(" + colors[0] + ", " + chosenColor + ")";
+    console.log(chosenColor);
+  });
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function listenForRandomize() {
+  let rnd_btn = document.getElementById("bg__random-btn");
+  rnd_btn.addEventListener("click", () => {
+    let color1 =
+      "rgb(" +
+      getRandomInt(255) +
+      "," +
+      getRandomInt(255) +
+      "," +
+      getRandomInt(255) +
+      ")";
+    let color2 =
+      "rgb(" +
+      getRandomInt(255) +
+      "," +
+      getRandomInt(255) +
+      "," +
+      getRandomInt(255) +
+      ")";
+    setColorOne(color1);
+    setColorTwo(color2);
+    document.body.style.background =
+      "radial-gradient(" + color1 + ", " + color2 + ")";
+  });
+}
+
 listenForButtonOne();
 listenForButtonTwo();
+listenForColorPickerOne();
+listenForColorPickerTwo();
+listenForRandomize();
+setInitialColors();
